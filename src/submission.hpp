15e58bbdf3318d14de33fe2a0bf7a56b0a0ebd42
Daniel Chen <ddaanniieell.chen@gmail.com>
2026-09-19 18:16:43 -0400

    fix aliasing
    an attempt...

--- src/submission.hpp
+++ src/submission.hpp
@@ -8,7 +8,6 @@
 // The evaluation harness uses operator() to set initial conditions and to read
 // results; it never touches your internal storage. Keep this interface,
 // everything else is yours.
-
 class Grid {
 private:
   std::size_t rows_;
@@ -37,6 +36,14 @@ public:
   std::size_t obtain_rows() const{
     return rows_;
   }
+
+  double* obtain_data() {
+    return data_matrix.data();
+  }
+
+  const double* obtain_data() const {
+    return data_matrix.data();
+  }
 };
 
 // Apply the five-point stencil over all interior points, copying the boundary
@@ -46,10 +53,16 @@ void apply_stencil(const Grid& old_grid, Grid& new_grid){
   std::size_t rows = old_grid.obtain_rows(); //i
 
   for (std::size_t i = 1; i < rows - 1; i++){
+    const double* __restrict row_mid = old_grid.obtain_data() + i * columns;
+    const double* __restrict row_up = old_grid.obtain_data() + (i - 1) * columns;
+    const double* __restrict row_down = old_grid.obtain_data() + (i + 1) * columns;
+    double* __restrict out = new_grid.obtain_data() + i * columns;
+
+    #pragma omp simd
     for (std::size_t j = 1; j < columns - 1; j++){
-      new_grid(i,j) = 0.5 * old_grid(i,j) +
-                   0.125 * (old_grid(i-1,j) + old_grid(i+1,j) +
-                            old_grid(i,j-1) + old_grid(i,j+1));
+      out[j] = 0.5 * row_mid[j] +
+                   0.125 * (row_down[j] + row_up[j] +
+                            row_mid[j-1] + row_mid[j+1]);
     }
   }
 
