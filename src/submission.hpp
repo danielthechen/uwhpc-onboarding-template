bf4524a8c8209c70f7f6c1a22ba1551bb7456935
Daniel Chen <ddaanniieell.chen@gmail.com>
2026-09-22 21:31:07 -0400

    added aligned flag to SIMD

--- src/submission.hpp
+++ src/submission.hpp
@@ -92,7 +92,7 @@ void apply_stencil(const Grid& old_grid, Grid& new_grid){
     out[0] = row_mid[0];
     out[columns - 1] = row_mid[columns - 1];
 
-    #pragma omp simd
+    #pragma omp simd aligned(row_mid, row_up, row_down, out: 64)
     for (std::size_t j = 1; j < columns - 1; j++){
       out[j] = 0.5 * row_mid[j] +
              0.125 * (row_down[j] + row_up[j] +
